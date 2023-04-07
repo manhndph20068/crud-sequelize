a009cc9b18e9ec258a157076a8236c5d97bf264e
manh <manhndph20068@fpt.edu.vn>
2023-04-07 21:52:19 +0700

    orm init

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from "express";
-import configViewEngine from "./configs/viewEngine";
+import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from "body-parser";
+import connection from "./config/connectDB";
 
 const app = express();
 configViewEngine(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//test connect db
+connection();
 
 initWebRoutes(app);
 
